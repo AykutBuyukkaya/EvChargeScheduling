d9a4b5f71b5ae35d4c37971709c1328dbdb2e708
Aykut Büyükkaya <aykutbkaya@gmail.com>
2023-02-25 15:36:31 +0300

    feat: Field annotations for ev data service mongodb.

--- EvDataService/src/main/java/com/buyukkaya/evdataservice/domain/model/entity/Ev.java
+++ EvDataService/src/main/java/com/buyukkaya/evdataservice/domain/model/entity/Ev.java
@@ -20,23 +20,31 @@ public class Ev implements Serializable {
     @Id
     private String id;
 
-    @Field(name = "brand_id")
+    @Field("brand_id")
     private String brandId;
 
-    private VehicleType vehicleType;
+    @Field("vehicle_type")
+    private String vehicleType;
 
+    @Field("model")
     private String model;
 
+    @Field("release_year")
     private String releaseYear;
 
+    @Field("variant")
     private String variant;
 
+    @Field("usable_battery_size")
     private Double usableBatterySize;
 
+    @Field("average_consumption")
     private Double averageConsumption;
 
+    @Field("ac_charger")
     private BasicDBObject acCharger;
 
+    @Field("dc_charger")
     private BasicDBObject dcCharger;
 
 
